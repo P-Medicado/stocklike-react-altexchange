a184189e0768be69da1d8f0e561c16ef52fda3d9
Dean Aikman <deanaikman@Deans-MacBook-Pro.local>
2017-09-29 18:36:24 -0500

    start

---
+++ src/index.js
@@ -0,0 +1,32 @@
+ 
+import React from 'react';
+import { render } from 'react-dom';
+import Chart from './Chart';
+import { getData } from "./utils";
+
+import { TypeChooser } from "react-stockcharts/lib/helper";
+
+
+class ChartComponent extends React.Component {
+	componentDidMount() {
+		getData().then(data => {
+			this.setState({ data })
+		})
+	}
+	render() {
+		if (this.state == null) {
+			return <div>Loading...</div>
+		}
+		return (
+			// <TypeChooser>
+				// {type => <div>{type}<Chart type="hybrid" data={this.state.data} /></div>}
+			// </TypeChooser>
+			<Chart type="hybrid" data={this.state.data} />
+		)
+	}
+}
+
+render(
+	<ChartComponent />,
+	document.getElementById("root")
+);
